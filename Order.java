bbcb5061cf9254f0c8977dbd140996e1293cbf4f
Mohammad Reza Qaderi <mrq112775@outlook.com>
2020-08-01 11:31:04 +0430

    The Order Class That Client Want From Restaurant

---
+++ Order.java
@@ -0,0 +1,31 @@
+/**
+ * This class have the order of the Client
+ *
+ * @author mohammad reza Qaderi
+ * @version 1.0
+ */
+public class Order {
+    //The Order Orde
+    private String orde;
+
+    /**
+     * Get the order
+     *
+     * @return order
+     */
+    public String getOrde() {
+
+        return orde;
+    }
+
+    /**
+     * Set the order
+     *
+     * @param orde of client
+     */
+    public void setOrde(String orde) {
+
+        this.orde = orde;
+    }
+
+}
